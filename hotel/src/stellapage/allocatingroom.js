66818e1b22b5673bad6bc5e30d199ab292833b86
Lenovo <Lenovo@DESKTOP-4L3DSQ5>
2023-09-15 21:37:11 +0530

    add.

--- hotel/src/stellapage/allocatingroom.js
+++ hotel/src/stellapage/allocatingroom.js
@@ -3,30 +3,88 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 
 const Allocateroom = () => {
-      return (
-        <Form>
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-            <Form.Label column sm="2">
-              Email
-            </Form.Label>
-            <Col sm="10">
-            <Form.Control type="email" placeholder="Email@gmail.com" />
-            </Col>
-          </Form.Group>
-    
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-            <Form.Label column sm="2">
-              Password
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control type="password" placeholder="Password" />
-            </Col>
-          </Form.Group>
-        </Form>
-      );
-    }
-  
+  const [showTable, setShowTable] = useState(false);
+function handleshowTable(){
+  setShowTable(!showTable)
+}
+  return (<>
+    <div><Button variant='primary'  className="left-aligned-button" onClick={handleshowTable}>Show Details</Button> </div>
+
+
+    <div style={{border:"2px solid"}} id="form">
+    <Form  className="form-floating mb-3 mt-3" >
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+        <Form.Label column sm="2" className='label-control'>
+          Student Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" placeholder="Student Name" required/>
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPhoneNumber">
+        <Form.Label column sm="2" className='label-control'>
+          Phone Number
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" placeholder="Phone Number" pattern="[0-9]{10}" required title='10 Digits only'/>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomType">
+        <Form.Label column sm="2" className='label-control'>
+          Room Type
+        </Form.Label>
+        <Col sm="10">
+          <Form.Select aria-label="Default select example" className="mb-3" >
+            <option disabled style={{backgroundColor:"white"}}>Open this select menu</option>
+            <option value="1" style={{backgroundColor:"white"}}>Single</option>
+            <option value="2" style={{backgroundColor:"white"}}>Double</option>
+            <option value="3" style={{backgroundColor:"white"}}>More(5 Members Only)</option>
+          </Form.Select>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomNumber" >
+        <Form.Label column sm="2" className='label-control'>
+          Room Number
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" placeholder="Room Number" required/>
+        </Col>
+      </Form.Group>
+      <Button type="submit" variant='success'>Submit</Button>
+    </Form>
+    </div>
+    <div>
+      {showTable&&
+    <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>S.NO</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            
+                        </tr>
+                </tbody>
+            </table>
+            }
+    </div>
+    </>
+  );
+}
+
 export default Allocateroom;
