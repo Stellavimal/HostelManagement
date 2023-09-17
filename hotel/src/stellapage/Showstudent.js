2a5bbbd7db1868e007986778d4104d829cac8385
Lenovo <Lenovo@DESKTOP-4L3DSQ5>
2023-09-17 23:32:43 +0530

    changes

--- hotel/src/stellapage/Showstudent.js
+++ hotel/src/stellapage/Showstudent.js
@@ -1,26 +1,52 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Modal, Button,Row,Col,Form } from 'react-bootstrap';
+import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 
 const StudentDetails = () => {
     const [details, setDetails] = useState([])
-   // const [newEntry, setNewEntry] = useState({ });
+    // const [newEntry, setNewEntry] = useState({ });
     const [showModal, setShowModal] = useState(false); // pop up wondow
     const [editIndex, setEditIndex] = useState(null);
-    const[input,setInput]=useState({ name: '', Roomtype: ''})
-
+    const [monthscal, setMoths] = useState();
+    const [input, setInput] = useState({ name: '', Roomtype: ''})
     const handleEdit = (details, index) => {
-       
+
         setEditIndex(index); // Set the index being edited
-        setInput({ ...input, name: details.name,Roomtype: details.Roomtype,});
-       
+        setInput({ ...input, name: details.name, Roomtype: details.Roomtype});
+
     };
     // const input = (event) => {
     //     const { name, value } = event.target;
     //     setNewEntry({ ...newEntry, [name]: value });
     // };
+    function dayscalculate(checkin, checkout) {
+        const fromdate = new Date(checkin);
+        const todate = new Date(checkout);
+      
+
+        const year1 = fromdate.getFullYear();
+        const year2 = todate.getFullYear();
+      
+        const month1 = fromdate.getMonth();
+        const month2 = todate.getMonth();
+      
+        const months = (year2 - year1) * 12 + (month2 - month1);
+        // setMoths(months)
+        return months;
+      }
+      
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`/api/rooms/${id}/`);
+            const updatedValues = details.filter(item => item.id !== id);
+            setDetails(updatedValues);
+            console.log(updatedValues)
+        } catch (error) {
+            console.error('Error deleting rooms:', error);
+        }
+    };
     useEffect(() => {
         async function fetchData() {
             try {
@@ -33,7 +59,7 @@ const StudentDetails = () => {
         }
 
         fetchData();
-    })
+    }, [])
 
     const design = {
         backgroundColor: "cadetblue"
@@ -50,6 +76,7 @@ const StudentDetails = () => {
                     <th style={design}>Check-In Date</th>
                     <th style={design}>Check-Out Date</th>
                     <th style={design}>RoomType</th>
+                    <th style={design}>No.of Months</th>
                     <th style={design}>Booking</th>
                 </tr>
             </thead>
@@ -61,54 +88,62 @@ const StudentDetails = () => {
                         <td>{item.checkin}</td>
                         <td>{item.checkout}</td>
                         <td>{item.Roomtype}</td>
-                        <td><Button variant="success"  onClick={() => {handleEdit(item, index); openModal();}}>
+                        <td>{dayscalculate(item.checkin, item.checkout)} Months</td>
+                        <td><Button variant="success" onClick={() => { handleEdit(item, index); openModal(); }}>
                             <i className="fas fa-bed"></i>
                         </Button>
-                            <Button variant="danger">
+                            <Button variant="danger" onClick={() => handleDelete(item.id)}>
                                 <i className="fas fa-trash"></i>
                             </Button></td>
                     </tr>
                 ))}
-
-
             </tbody>
         </table>
         <Modal show={showModal} onHide={closeModal}>
-            <Modal.Header closeButton style={{backgroundColor:"cadetblue"}}>
+            <Modal.Header closeButton style={{ backgroundColor: "cadetblue" }}>
                 <Modal.Title> Booking Details</Modal.Title>
             </Modal.Header>
-            <Modal.Body style={{border:"2px solid #007bff"}}>
-            <Form  className="form-floating mb-3 mt-3" >
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
-        <Form.Label column sm="2" className='label-control'>
-          Student Name
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="text" placeholder="Student Name" required value={input.name}/>
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomType">
-        <Form.Label column sm="2" className='label-control'>
-          Room Type
-        </Form.Label>
-        <Col sm="10">
-        <Col sm="10">
-          <Form.Control type="text" placeholder="Room Type" required value={input.Roomtype}/>
-        </Col>
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomNumber" >
-        <Form.Label column sm="2" className='label-control'>
-          Room Number
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="text" placeholder="Room Number" required/>
-        </Col>
-      </Form.Group>
-     
-    </Form>
+            <Modal.Body style={{ border: "2px solid #007bff" }}>
+                <Form className="form-floating mb-3 mt-3" >
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+                        <Form.Label column sm="2" className='label-control'>
+                            Student Name
+                        </Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="text" placeholder="Student Name" required value={input.name} />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomType">
+                        <Form.Label column sm="2" className='label-control'>
+                            Room Type
+                        </Form.Label>
+                        <Col sm="10">
+                            <Col sm="10">
+                                <Form.Control type="text" placeholder="Room Type" required value={input.Roomtype} />
+                            </Col>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomNumber" >
+                        <Form.Label column sm="2" className='label-control'>
+                            Room Number
+                        </Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="text" placeholder="Room Number" required />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextmonth" >
+                        <Form.Label column sm="2" className='label-control'>
+                           Total Months
+                        </Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="text" placeholder="Months" required value={input} />
+                        </Col>
+                    </Form.Group>
+
+
+                </Form>
             </Modal.Body>
-            <Modal.Footer style={{border:"2px solid #007bff"}}>
+            <Modal.Footer style={{ border: "2px solid #007bff" }}>
                 (<Button variant="primary">
                     Book</Button>)
                 <Button variant="secondary" onClick={closeModal}>
