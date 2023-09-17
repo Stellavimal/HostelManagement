2e9db3ba4fa501e212da207b82fbf7d0135d6207
Lenovo <Lenovo@DESKTOP-4L3DSQ5>
2023-09-17 21:14:47 +0530

    add

--- hotel/src/stellapage/Showstudent.js
+++ hotel/src/stellapage/Showstudent.js
@@ -1,10 +1,26 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Button from 'react-bootstrap/Button';
+import { Modal, Button,Row,Col,Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 
 const StudentDetails = () => {
     const [details, setDetails] = useState([])
+   // const [newEntry, setNewEntry] = useState({ });
+    const [showModal, setShowModal] = useState(false); // pop up wondow
+    const [editIndex, setEditIndex] = useState(null);
+    const[input,setInput]=useState({ name: '', Roomtype: ''})
+
+    const handleEdit = (details, index) => {
+       
+        setEditIndex(index); // Set the index being edited
+        setInput({ ...input, name: details.name,Roomtype: details.Roomtype,});
+       
+    };
+    // const input = (event) => {
+    //     const { name, value } = event.target;
+    //     setNewEntry({ ...newEntry, [name]: value });
+    // };
     useEffect(() => {
         async function fetchData() {
             try {
@@ -19,9 +35,12 @@ const StudentDetails = () => {
         fetchData();
     })
 
-    const design={
-        backgroundColor:"cadetblue"
+    const design = {
+        backgroundColor: "cadetblue"
     }
+    const openModal = () => setShowModal(true);
+    const closeModal = () => setShowModal(false);
+
     return (<div>
         <table className="table table-bordered">
             <thead>
@@ -42,10 +61,10 @@ const StudentDetails = () => {
                         <td>{item.checkin}</td>
                         <td>{item.checkout}</td>
                         <td>{item.Roomtype}</td>
-                        <td><Button  variant="success">
+                        <td><Button variant="success"  onClick={() => {handleEdit(item, index); openModal();}}>
                             <i className="fas fa-bed"></i>
                         </Button>
-                            <Button  variant="danger">
+                            <Button variant="danger">
                                 <i className="fas fa-trash"></i>
                             </Button></td>
                     </tr>
@@ -54,7 +73,51 @@ const StudentDetails = () => {
 
             </tbody>
         </table>
-        
+        <Modal show={showModal} onHide={closeModal}>
+            <Modal.Header closeButton style={{backgroundColor:"cadetblue"}}>
+                <Modal.Title> Booking Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={{border:"2px solid #007bff"}}>
+            <Form  className="form-floating mb-3 mt-3" >
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+        <Form.Label column sm="2" className='label-control'>
+          Student Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" placeholder="Student Name" required value={input.name}/>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomType">
+        <Form.Label column sm="2" className='label-control'>
+          Room Type
+        </Form.Label>
+        <Col sm="10">
+        <Col sm="10">
+          <Form.Control type="text" placeholder="Room Type" required value={input.Roomtype}/>
+        </Col>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextRoomNumber" >
+        <Form.Label column sm="2" className='label-control'>
+          Room Number
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" placeholder="Room Number" required/>
+        </Col>
+      </Form.Group>
+     
+    </Form>
+            </Modal.Body>
+            <Modal.Footer style={{border:"2px solid #007bff"}}>
+                (<Button variant="primary">
+                    Book</Button>)
+                <Button variant="secondary" onClick={closeModal}>
+                    Close
+                </Button>
+
+            </Modal.Footer>
+        </Modal>
+
     </div >);
 }
 
