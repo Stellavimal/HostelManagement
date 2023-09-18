eb80f82fd08ca944739e47432f934ab4433229b6
salomi <lurdhu2003@gmail.com>
2023-09-18 15:15:21 +0530

    add

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,30 +1,22 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-<<<<<<< HEAD
 // import Allocateroom from './stellapage/allocatingroom';
 import Payment from './paymentsalo/payment';
 import Viewpay from './paymentsalo/showtable';
-
-function App() {
-  return (
-    <>
-    {/* // <><Allocateroom/> */}
-    <Payment/>
-    <Viewpay/>
-    </>
-    
-=======
 import Allocateroom from './stellapage/allocatingroom';
 import StudentDetails from './stellapage/Showstudent';
 
 function App() {
   return (
-    //  <><Allocateroom/>
+    <>
+    {/* // <><Allocateroom/> */}
     <StudentDetails/>
-   
->>>>>>> d879dc64339c994441cd68260c3ac8ca4aeca198
-  );
+    <Payment/>
+    <Viewpay/>
+    </>
+    
+  )
 }
 
 export default App;
